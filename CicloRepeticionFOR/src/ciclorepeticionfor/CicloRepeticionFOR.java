07c180316da28f42818a2de7ca62e49671998a69
MeniakGG <kevinjavierestrada@gmail.com>
2026-08-12 19:36:14 -0600

    ciclo FOR

--- CicloRepeticionFOR/src/ciclorepeticionfor/CicloRepeticionFOR.java
+++ CicloRepeticionFOR/src/ciclorepeticionfor/CicloRepeticionFOR.java
@@ -4,6 +4,8 @@
  */
 package ciclorepeticionfor;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kevin.estrada
@@ -15,6 +17,9 @@ package ciclorepeticionfor;
  * Variable de control
  * Condicion
  * Modificador
+ * 
+ * 
+ * 
  * 
  */
 public class CicloRepeticionFOR {
@@ -34,8 +39,41 @@ public class CicloRepeticionFOR {
             System.out.printf("Valor; %d\n", i);
         }
         
+        //Ejercicio 
+        //Variables
+        Scanner sc = new Scanner (System.in);
+        final int TOTAL = 10;
+        int nomVarones = 0;
+        int numMujeres = 0;
+        char seleccion = 'a';
+        double porcentaje = 0;
         
+        for(int i = 0; i < TOTAL; i++){
+            System.out.println("Quien viene x minuta");
+            System.out.println("V Varon | M Mujer");
+            System.out.println("Respuesta:");
+            seleccion = sc.next().toUpperCase().charAt(0);
+            switch (seleccion){
+               case 'V':
+                    System.out.println("Varon comiendo minutas");
+                    nomVarones++;
+               break;
+               case 'M':
+                   System.out.println("Mujer comindo minuta");
+                   numMujeres++;
+               break;
+               default:
+                   System.out.println("Seleccion no valida");
+               break;
+            }    
+    }
+        System.out.println("Porcentaje de Varones\n");
+        porcentaje = nomVarones / TOTAL;
+        System.out.printf("Porcentaje %.2f\n",porcentaje);
         
+        System.out.println("Porcentaje de Mujeres\n");
+        porcentaje = numMujeres / TOTAL;
+        System.out.printf("Porcentaje %.2f\n",porcentaje);
     }
     
 }
