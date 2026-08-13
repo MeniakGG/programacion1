b886f88ff076f70f9f9fa256755031f3e411a35d
MeniakGG <kevinjavierestrada@gmail.com>
2026-08-12 20:45:22 -0600

    Calculo de temperatura

---
+++ SistemaMonitoreo/src/sistemamonitoreo/SistemaMonitoreo.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemamonitoreo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kevin.estrada
+ * * Desarrolla un programa en Java que simule la lectura de sensores de 
+     * temperatura en un cuarto de servidores. El programa debe solicitar 
+     * el nombre del operador encargado y cuántas lecturas de sensores se 
+     * van a simular. Debe validar que la cantidad de lecturas sea mayor a 0 
+     * y menor o igual a 50. Por cada lectura, el sistema generará una temperatura 
+     * aleatoria en grados Celsius. El programa debe convertir cada lectura a 
+     * Fahrenheit, determinar si hay una alerta de sobrecalentamiento y, al final, 
+     * mostrar un resumen estadístico.
+     * 
+     * 1 comprender el problema que se está abordando
+     *  a que datos de entrada necesito
+     *  b transpormación/calculos necesarios
+     *  c Que resultados se presentan?
+     * 
+     * 2 Visualizacion / diseño de la solucion
+     *  a diagrama flujo
+     *  b pseudocodigo
+     * 
+     * 3 programacion incremental
+     *  a listado de tareas a cumplir
+     *      i Declarancion de variables
+     *      ii Estructuras de seleccion
+     *      iii Ciclo de repeticion
+     *      iV Validaciones de datos
+     * 
+     * 4 Prueba de escritorio
+     *  a casos
+     *  b escenarios
+     *  c resultados / Patrones
+     * 
+     * Entradas:
+     *  Nombre de Usaurio
+     *  Numero de Lecturas
+     *  Ingreso de tempraturas (Random)
+     * 
+     * Calculos / Transformaciones
+     *  Formula de Celsius a Farenheit
+     *  Definicion de sobrecalentamiento
+     *  Validacion Lecturas (0 > lectura menor 50)
+     *  Contabilizar el numeto de calentamiento
+     *  Promediar el monitoreo
+     *  - Validacion Temperatura
+     * 
+     * Salida
+     *  Resumen estadistico
+     *  Alerta de Sobrecalentamiento
+     * 
+     * Tareas
+     * 1 Capturar el nombre del usuario
+     *  a Definicion de librería/ Instancia
+     *  b Declaracion de Variable para el usuario
+     * 2 Captura de Numero de lecturas
+     *  a Declaracion de Variable para numero de lectura
+     * 3 Generacion de temperatura
+     *  A definicion de librería RANDOM
+     *  b Declaracion de Variable
+     *  c Mostrar el numero Generado Temporalmente
+     * 
+ */
+public class SistemaMonitoreo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Scanner HERE
+        Scanner sc = new Scanner (System.in);
+        // Variables aqui
+        String nomoperador = "mm";
+        int lecturas = 0;
+        int temperaturas = 0;
+        
+        
+        
+        
+       
+   
+    
+    } // Fin Main
+} // Fin Public Class
