f751f69697167caa2cfa3ee207ab239b63f6c7d9
MeniakGG <kevinjavierestrada@gmail.com>
2026-08-14 20:57:06 -0600

    Sistema de monitoreo

--- SistemaMonitoreo/src/sistemamonitoreo/SistemaMonitoreo.java
+++ SistemaMonitoreo/src/sistemamonitoreo/SistemaMonitoreo.java
@@ -4,6 +4,7 @@
  */
 package sistemamonitoreo;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -66,7 +67,15 @@ import java.util.Scanner;
      *  A definicion de librería RANDOM
      *  b Declaracion de Variable
      *  c Mostrar el numero Generado Temporalmente
-     * 
+     *  d rango de temp 200 - 400
+     * 4 Formula: F = (C * 9/5) + 32
+     * 5 Limite de sobrecalentamiento 350C (662 F)
+     * 6 Validacion de lecturas (Do While) Entre 0 y 50
+     * 7 Sumar los numero de Sobrecalentaminto
+     * 8 Calcular el promedio
+     * 9 Mostrar el resumen
+     *   - Promedio
+     *   - Numero de sobrecalentamientos
  */
 public class SistemaMonitoreo {
 
@@ -74,18 +83,61 @@ public class SistemaMonitoreo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Scanner HERE
+        // Scanner y Random HERE 
         Scanner sc = new Scanner (System.in);
+        Random rd = new Random ();
+        
         // Variables aqui
-        String nomoperador = "mm";
+        String nomusuario = "mm";
         int lecturas = 0;
         int temperaturas = 0;
+        int ciclo = 0;
+        double TempF = 0;
+        double TempC = 0;
+        int SobrecanL =0;
+        double acumTem = 0;
+        double promedio = 0;
+                
+        // Print y captura de datos
+        System.out.print("Nombre de operario: ");
+        nomusuario = sc.nextLine().toUpperCase();
+        System.out.printf("Validar Captura %s\n", nomusuario);
+        
+        do{
+        System.out.print("Numero de Lectura: ");
+        lecturas = sc.nextInt();
+        System.out.printf("Validacion Captura: %d\n", lecturas);
+        if (lecturas <0 || lecturas > 50){
+            System.out.println("Valor no Valido");
+        }
+        }while(lecturas < 0 || lecturas > 50);
         
         
+        //TempC = rd.nextDouble();
+        TempC = rd.nextDouble(200,500);
+        //TempC = (Math.random()*400)-200 + 200;
+        System.out.printf("Validacion Random %.2f\n", TempC);
         
-        
-       
-   
-    
+        // While Here
+        while (ciclo < lecturas){
+             TempC = rd.nextDouble(200,500);
+             TempF = (TempC*9/5) + 32;
+        //TempC = (Math.random()*400)-200 + 200;
+            System.out.printf("Num Lec: %d\ttemp C: %.2f\n",ciclo+1,TempC);
+            System.out.printf("Temp F: %.2f\n", TempF);
+            ciclo ++;
+            if (TempF>662){
+                System.out.println("Alerta!!! Sobrecalentamiento");
+                SobrecanL++;
+            }else
+                acumTem += TempF;
+            
+        } //Fin While
+        promedio = acumTem / lecturas;
+        System.out.println("------------------------------------");
+        System.out.println("          RESUMEN                   ");
+        System.out.printf("Promedio de TEMP: %.2f\n", promedio);
+        System.out.printf("# sobrecalentamientos: %d\n",SobrecanL);
+
     } // Fin Main
 } // Fin Public Class
